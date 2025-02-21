188122c34f091c5190f1a8f6371ee5dba6e54b89
alissaryb <alissaryb@gmail.com>
2025-02-21 14:20:26 +0300

    6

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, request
+import requests
 from random import choice
 
 app = Flask(__name__)
